48263d7e90887eb0c4026fcc5179922c533f506d
carigar <carigar4@alumni.uv.es>
2024-10-16 09:31:09 +0200

    Perímetro rectángulo y cambio PI

--- Perimetros.R
+++ Perimetros.R
@@ -2,5 +2,8 @@ PerimetroCuadrado<-function(L){
   return(4*L)
 }
 LongitudCircunferencia<- function(R){
-  return(2*pi*R)
+  return(2*3.14159*R)
+}
+PerimetroRectangulo<-function(L1,L2){
+  return(2*(L1+L2))
 }
